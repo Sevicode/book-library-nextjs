1be10666cf1b771a23c4469ea03751f2eb1f4e71
Sevda <sevda.amini@gmail.com>
2022-02-08 20:51:28 +1000

    static props added

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Layout from '../components/Layout'
+
+
 
 export default function Home() {
   return (
-    <div>
+    <Layout>
+  
      
-    </div>
+    </Layout>
   )
 }
