046fd9f6499f86656bb55b70fd78414aab898f4a
Sevda <sevda.amini@gmail.com>
2022-02-08 20:34:35 +1000

    header and footer created

---
+++ components/Footer.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+import styles from "@/styles/Footer.module.css";
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p>Copyright &copy; Art Fun 2022, Created by Sevinu</p>
+    </footer>
+  );
+}
