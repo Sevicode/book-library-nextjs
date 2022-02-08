c39cfeb17958e1d437eca299fb8c63ed3b43de52
Sevda <sevda.amini@gmail.com>
2022-02-08 21:01:15 +1000

    jsconfig added

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,21 @@ import Layout from '../components/Layout'
 export default function Home() {
   return (
     <Layout>
+      <h1>Latest Releases</h1>
   
      
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL/api/books}`)
+  const books = await res.json()
+  return {
+    props: {books: books.slice(0, 7)},
+    revalidate: 1,
+  }
+   
+
+  
 }
