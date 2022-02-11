e23e0004b09d6185b372a0f5b7978f2707450902
Sevda <sevda.amini@gmail.com>
2022-02-11 12:31:10 +1000

    bookiTEMS added

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,32 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Layout from '../components/Layout'
+import Link from "next/link";
+import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
+import BookItems from "@/components/BookItems";
 
-
-
-export default function Home() {
+export default function Home(books) {
   return (
     <Layout>
       <h1>Latest Releases</h1>
-  
-     
+      {books.length === 0 && <h3>No Result Available</h3>}
+
+      {books.map((book) => (
+        <BookItems key={book.id} book={book} />
+      ))}
+
+      {books.length > 0 && (
+        <Link href="/books">
+          <a className="btn-secondary">View All Books</a>
+        </Link>
+      )}
     </Layout>
-  )
+  );
 }
 
 export async function getStaticProps() {
-  const res = await fetch(`${API_URL/api/books}`)
-  const books = await res.json()
+  const res = await fetch(`${API_URL / api / books}`);
+  const books = await res.json();
   return {
-    props: {books: books.slice(0, 7)},
+    props: { books: books.slice(0, 3) },
     revalidate: 1,
-  }
-   
-
-  
+  };
 }
